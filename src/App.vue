d6889394ec38303cf413b1019f3405a03aea2e9a
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-08 20:04:56 +0800

    建立一個Local filter!

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{ someText | toUppercase }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                someText: 'Hello there!'
+            }
+        }, 
+        filters: {
+            // filter 的 value 由 | 前面執行的結果傳進來
+            toUppercase: function(value) { // 也可以寫成 toUppercase: function(value) {} 
+                return value.toUpperCase();
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
