12e1e967c00c3d8b409f3567c40012badfb25222
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-08 21:08:56 +0800

    Array要如何實作filter?用 computed,把computed後的array 呈現出來

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
                 <h1>Filters & Mixins</h1>
                 <p>這個會被轉大寫：{{ someText | toUppercase }}</p>
                 <p>這個最後會被轉小寫：{{ someText | toUppercase | to-lowercase}}</p>
+
+                <hr>
+                <!-- 針對 Array 要如何實作 filter? 用 computed function!! 也就是把 computed後的array 呈現出來即可 -->
+                針對 array 實作 filter<br>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="(fruit, index) in filterFruits" :key="index">{{ fruit }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -14,13 +22,22 @@
     export default {
         data: function() {
             return {
-                someText: 'Hello there!'
+                someText: 'Hello there!',
+                fruits: ['Apple', 'Mongo', 'Banana'],
+                filterText: ''
             }
         }, 
         filters: {
             // filter 的 value 由 | 前面執行的結果傳進來
             toUppercase: function(value) { // 也可以寫成 toUppercase: function(value) {} 
                 return value.toUpperCase();
+            }
+        },
+        computed: {
+            filterFruits: function() { // 也可以寫成 filterFruits() {}
+                return this.fruits.filter( (element) => { // 注意！不可以寫成 function(element) {}
+                    return element.match(this.filterText);
+                });
             }
         }
     }
