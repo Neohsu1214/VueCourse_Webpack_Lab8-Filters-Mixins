c6d2bb889cb5bf7030ada5a1b7b2929cfb23b81f
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-08 20:12:11 +0800

    註冊一個 global filter

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 
 // filter是一個工具，能在不異動原有資料的情況下，處理如何顯示的議題 ex: 1000 -> 1,000
 // 註冊一個 GLobal 的 filter: to-lowercase
-Vue.filter('to-lowercase', {
-  
+Vue.filter('to-lowercase', function(value) {
+  return value.toLowerCase();
 });
 
 new Vue({
