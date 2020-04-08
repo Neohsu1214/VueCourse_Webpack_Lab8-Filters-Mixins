ccd750d4421178256c59127df24480236a5de529
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-08 20:12:33 +0800

    串聯使用 filter 之例子

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ someText | toUppercase }}</p>
+                <p>這個會被轉大寫：{{ someText | toUppercase }}</p>
+                <p>這個最後會被轉小寫：{{ someText | toUppercase | to-lowercase}}</p>
             </div>
         </div>
     </div>
