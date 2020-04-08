77e9e669c3810bedb09413a2b853e87b26e36bd6
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-08 22:04:58 +0800

    Global Mixin會自動加載於各個 Vue instance中！共用的企業邏輯，都可以宣告在此

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ Vue.filter('to-lowercase', function(value) {
   return value.toLowerCase();
 });
 
+// 註冊一個 global mixin!
+Vue.mixin({
+  created: function() {
+    console.log('Global Mixin - created hook');
+    // 查看 log 會發現... Global Mixin會自動加載於各個 Vue instance中！！！
+    // Neo案： 感覺好像病毒 XD 但有任何要 global 使用的企業邏輯，都可以宣告在 Global Mixin中自動加載！甚好啊！
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
